6a8ad70bcfba1326eab78d524da791b81d1ff576
Martin Kralev <martin.kralev@mail.utoronto.ca>
2019-02-16 17:24:47 -0500

    Starter code for all of our functions

---
+++ sudoku_game.py
@@ -0,0 +1,42 @@
+from random import randint
+
+
+def generate_grid():
+    #create grid and return list of size 81
+
+
+def print_grid(lst_of_ints):
+    # Prints grid, returns void.
+
+
+def user_select_difficulty(list_selected_diff):
+    # Prompt user to select difficulty, returns the corresponding integer.
+
+
+def create_whitespaces(input_lst, dic, difficulty):
+    # Takes in list and creates difficulty amount of whitespaces, and saves them in a dictionary, returns new list.
+
+
+def helper_letter_coordinate(s):
+    # Changes coordinates into integer, return 0-80 integer.
+
+
+def check_can_modify_coord(dic, index1):
+    # Acesses dictionary to check if coordinate selected by user is modifiable, return boolean answer.
+
+
+def check_value_in_digit(lst_to_fill, index2):
+    # Prompt user for digit they want to input at given index, check if it a digit, return the list with changes.
+
+
+def check_solve(result_string, latest_input, lst1, lst2):
+    # Check if user has won. Print the answer.
+
+
+def play_game(difficulty_selected):
+    # Play game. Loop through possibilities.
+
+
+if __name__ == '__main__':
+    # Generate, select diffuclty and play.
+
