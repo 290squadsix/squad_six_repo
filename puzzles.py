8c5338973a3d1940e3634a029f1b5e41c3eb42c9
Martin Kralev <martin.kralev@mail.utoronto.ca>
2019-03-15 15:34:08 -0400

    Implement mapping algorithm for better randomization of grids

--- puzzles.py
+++ puzzles.py
@@ -1,7 +1,9 @@
-from random import randint
-def get_random_puzzle():
+from random import *
+
+
+def get_grid():
     ''' Return a random puzzle '''
-    x = randint(1,3) # set second number to total number of puzzles
+    x = randint(1, 3)  # set second number to total number of puzzles
 
     puzzle_1 =  [1, 2, 3, 4, 5, 6, 7, 8, 9,
                  4, 5, 6, 7, 8, 9, 1, 2, 3,
@@ -45,3 +47,58 @@ def get_random_puzzle():
         return puzzle_2
     if x == 3:
         return puzzle_3
+
+
+def helper_digits(lst):
+
+    x = randint(1, 9)
+    while x in lst:
+        x = randint(1, 9)
+    lst.append(x)
+
+    return x
+
+
+def get_random_puzzle():
+
+    lst = get_grid()
+
+    digits = []
+
+    a = helper_digits(digits)
+    b = helper_digits(digits)
+    c = helper_digits(digits)
+    d = helper_digits(digits)
+    e = helper_digits(digits)
+    f = helper_digits(digits)
+    g = helper_digits(digits)
+    h = helper_digits(digits)
+    z = helper_digits(digits)
+
+    i = 0
+
+    while i < 81:
+
+        if lst[i] == 1:
+            lst[i] = a
+        elif lst[i] == 2:
+            lst[i] = b
+        elif lst[i] == 3:
+            lst[i] = c
+        elif lst[i] == 4:
+            lst[i] = d
+        elif lst[i] == 5:
+            lst[i] = e
+        elif lst[i] == 6:
+            lst[i] = f
+        elif lst[i] == 7:
+            lst[i] = g
+        elif lst[i] == 8:
+            lst[i] = h
+        elif lst[i] == 9:
+            lst[i] = z
+        i += 1
+
+    print(lst)
+    return lst
+
